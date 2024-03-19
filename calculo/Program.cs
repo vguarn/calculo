823f46be06950e8a48a7ba5916f67778b35fa88f
Usuario <Usuario@CPD02-M14>
2024-03-19 10:50:17 -0300

    Adicionar arquivos de projeto.

---
+++ calculo/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o primeiro número: ");
+             double n1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o segundo número: ");
+             double n2 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Escolha a operação: +, -, *, /");
+             string operacao = Console.ReadLine();  
+
+            switch (operacao)
+            {
+                case "+":
+                    double resultado = n1 + n2;
+                    Console.WriteLine("O resultado da operacão é: " + resultado);
+                break;
+
+                case "-":
+                    double resultado2 = n1 - n2;
+                    Console.WriteLine("O resultado da operacão é: " + resultado2);
+                break;
+
+                case "*":
+                    double resultado3 = n1 * n2;
+                    Console.WriteLine("O resultado da operacão é: " + resultado3);
+                break;
+
+                case "/":
+                    double resultado4 = n1 + n2;
+                    Console.WriteLine("O resultado da operacão é: " + resultado4);
+                break;
+            }
+        }
+    }
+}
